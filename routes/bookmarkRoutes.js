1c97bb1e3308d66720811f66b35a6896028495f6
sanya2701 <sanyagupta225@gmail.com>
2020-05-07 22:11:17 +0530

    Add, remove tags from bookmark

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -59,7 +59,7 @@ router.get("/:id",(req,res)=>{
     .populate('tag')
     .exec((err,bm)=>{
         if(err) {
-             console.log(err);
+             console.log(err); 
         } else {
             //console.log(bm);
             res.render("showUniqBm",{
@@ -85,6 +85,53 @@ router.delete("/:id",(req,res)=>{
              });
     }
     });
+})
+
+//Add tag to bookmark render page
+router.get("/:id/addTagToBM",(req,res)=>{
+    Bookmark.findById(req.params.id,(err,bm)=>{
+        if(err){
+            console.log(err);
+        }else{
+            Tag.find({},(err,tag)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    //console.log(tag);
+                    //console.log(bm);
+                    res.render("listOfTags",{tag:tag,bm:bm});
+                }
+            })
+        }
+    })
+})
+
+//Add tag to bookmark post route
+router.post("/:id/addTagToBM",(req,res)=>{
+    console.log(req.body);
+    const bm = {}
+    bm.tag = req.body.btag;
+    Bookmark.updateOne({_id:req.params.id},bm,(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/api/bookmark/"+req.params.id);
+        }
+    })
+})
+
+//Delete tag from bookmark
+router.post("/:id/delTag",(req,res)=>{
+    console.log(req.body);
+    const bm = {}
+    bm.tag = '507f191e810c19729de860ea';
+    Bookmark.updateOne({_id:req.params.id},bm,(err)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/api/bookmark/"+req.params.id);
+        }
+    })
 })
 
 module.exports = router;
